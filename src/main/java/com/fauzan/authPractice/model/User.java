608571c31306aa650984cdf23980291d3e05070f
Fozant <vrozh7@gmail.com>
2024-01-23 16:03:51 +0700

    add model, update xml

---
+++ src/main/java/com/fauzan/authPractice/model/User.java
@@ -0,0 +1,35 @@
+package com.fauzan.authPractice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+    
+    private String password;
+    
+
+
+}
